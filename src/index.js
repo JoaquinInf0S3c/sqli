a658a9b9060f5ac0420a5bd9232f49979086460a
JoaquinAO <joaquinaravena@protonmail.com>
2023-06-16 18:09:43 -0400

    added login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const connection = require("./connection");
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.sendFile("index.html", { root: __dirname });
@@ -16,6 +20,33 @@ app.get("/product", (req, res) => {
     } else {
       console.log(err);
       res.status(400).send(fields);
+    }
+  });
+});
+
+app.get("/login", (_req, res) => {
+  res.sendFile("login.html", { root: __dirname });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Verificar las credenciales del usuario en la base de datos
+  const query = `SELECT * FROM users WHERE name = '${username}' AND password = '${password}'`;
+  connection.query(query, (err, result) => {
+    if (!err && result.length > 0) {
+      const response = {
+        success: true,
+        message: "Login successful",
+        username: result[0].name,
+      };
+      res.status(200).json(response);
+    } else {
+      const response = {
+        success: false,
+        message: "Login failed",
+      };
+      res.status(401).json(response);
     }
   });
 });
